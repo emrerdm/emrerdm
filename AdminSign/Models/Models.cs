dd4a9508d9606941d1351b4a9c77ffb7562e1108
Emre Erdem <emreerdem4444@gmail.com>
2018-11-08 14:56:21 +0300

    first

---
+++ AdminSign/Models/Models.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminSign.Models
+{
+    public class Context : DbContext
+    {
+        public Context()
+        // C# will call base class parameterless constructor by default
+        {
+        }
+        public Context(DbContextOptions<Context> options) : base(options)
+         
+        {
+        }
+
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Customers> Customer { get; set; }
+    }
+
+    public class Categories
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+ 
+    }
+
+    public class Customers
+    {
+        [Key]
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+         
+    }
+}
---
+++ AdminSign/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+ 
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AdminSign
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+                var host = BuildWebHost(args);
+
+                host.Run();
+            }
+
+            // Tools will use this to get application services
+            public static IWebHost BuildWebHost(string[] args) =>
+                new WebHostBuilder()
+                    .UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .Build();
+
+            //using (var ctx = new Context())
+            //{
+            //    var stud = new Category() { CategoryName = "Bill" };
+
+            //    ctx.Category.Add(stud);
+            //    ctx.SaveChanges();
+            //}
+            //CreateWebHostBuilder(args).Build().Run();
+            //Context Data = new Context();
+            // Data.Category.Add(new Category
+            // {
+            //     CategoryName = "Test1",
+            //     Description = "Test2",
+            //     Picture="Test3"
+            // });
+            //Data.SaveChanges();
+            //Context Veri = new Context();
+            //Category ornek = Veri.Category.FirstOrDefault(o => o.CategoryID == 7);
+            //ornek.CategoryName = "Test1";
+            //ornek.Description = "Test2";
+            //ornek.Picture = "Test3";
+            //Veri.SaveChanges();
+        }
+
+        
+    }
+
